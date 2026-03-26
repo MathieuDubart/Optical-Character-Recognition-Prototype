f844a41237e9dc8b645dc2efafbcbc1accd7d5ec
Mathieu Dubart <mathieu.dubart@icloud.com>
2026-03-26 15:49:20 +0100

    Initial Commit

---
+++ OCR Prototype/OCR_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCR_PrototypeApp.swift
+//  OCR Prototype
+//
+//  Created by Mathieu Dubart on 26/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct OCR_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
